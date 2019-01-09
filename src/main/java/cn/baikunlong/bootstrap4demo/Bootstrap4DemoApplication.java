0b7e7e03bf6fa90a48861780bf78fa590f0f99ab
baikunlong <1262198701@qq.com>
2019-01-09 19:29:01 +0800

    创建项目

---
+++ src/main/java/cn/baikunlong/bootstrap4demo/Bootstrap4DemoApplication.java
@@ -0,0 +1,14 @@
+package cn.baikunlong.bootstrap4demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstrap4DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap4DemoApplication.class, args);
+    }
+
+}
+
